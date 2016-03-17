4d69d6e5c87701f636c7bdaaad55726ee407d7b1
ryancharris <harrisryan1@gmail.com>
2016-03-17 15:05:34 -0400

    Finished 'Handling Routes in Node'

---
+++ app.js
@@ -0,0 +1,21 @@
+// Problem: We need a simple way to look at a user's badge count and JavaScript points from a web browser
+
+// Solution: Use Node.js to perform the profile lookups and serve our template via HTTP
+
+var router = require("./router.js");
+
+// Create a web server
+var http = require("http");
+
+http.createServer(function (request, response) {
+  router.home(request, response);
+  router.user(request, response);
+}).listen(1337);
+
+console.log("Sever running at http://127.0.0.1:1337/");
+
+
+
+// Function that handles the reading of files and merge in values
+  // Read from file and get a string
+    // Merge values into string

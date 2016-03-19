aa20ea61ad0033f81e522544ba428458681ecfd3
ryancharris <harrisryan1@gmail.com>
2016-03-19 16:30:41 -0400

    Creating a Basic Template Engine in Node.js

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 var Profile = require("./profile.js");
-
+var renderer = require("./renderer.js");
 
 // Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
@@ -7,9 +7,9 @@ function home(request, response) {
   if(request.url === "/") {
     // show search
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write("Header\n");
-    response.write("Search\n");
-    response.end("Footer\n");
+    renderer.view("header", {}, response);
+    renderer.view("search", {}, response);
+    renderer.view("footer", {}, response);
   }
   // if URL == "/"" && POST
     // redirect to /username
@@ -23,7 +23,7 @@ function user(request, response) {
 
   if(username.length > 0) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write("Header\n");
+    renderer.view("header", {}, response);
 
     // get JSON from treehouse
     var studentProfile = new Profile(username);
@@ -40,8 +40,9 @@ function user(request, response) {
         javascriptPoints: profileJSON.points.JavaScript
       };
       // Simple response
-      response.write(values.username + " has " + values.badges + " badges.\n");
-      response.end("Footer\n");
+      renderer.view("profile", values, response);
+      renderer.view("footer", {}, response);
+      response.end();
 
     });
 
@@ -49,8 +50,10 @@ function user(request, response) {
     // on "error"
     studentProfile.on("error", function(error){
       // display error
-      response.write(error.message + "\n");
-      response.end("Footer\n");
+      renderer.view("error", {errorMessage: error.message}, response);
+      renderer.view("search", {}, response);
+      renderer.view("footer", {}, response);
+      response.end();
     });
 
   }
